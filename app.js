7a3d079f673a89d9d9379ca2664a570e8fb4a7b7
Per Nilsson <pnilsson@walmartlabs.com>
2013-08-03 09:30:43 -0700

    Add dev error handler

--- app.js
+++ app.js
@@ -89,6 +89,9 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
 app.get('/', function(req, res){
   res.redirect('/index.html');
