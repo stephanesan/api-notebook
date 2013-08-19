5ef88b597e4fde9873741fa3eb5e31dee5d12312
Per Nilsson <pnilsson@walmartlabs.com>
2013-08-19 13:37:18 -0700

    Dynamically generate ControlsView buttons based on model data

--- public/scripts/models/controls.js
+++ public/scripts/models/controls.js
@@ -1,3 +1,22 @@
 var Backbone = require('backbone');
 
-var Controls = module.exports = Backbone.Model.extend();
+var Controls = module.exports = Backbone.Model.extend({
+  actions: [{
+      name: "toggle-mode",
+      label: "Switch Mode",
+      icon: "file"
+    }, {
+      name: "add-cell",
+      label: "Add Cell",
+      icon: "plus"
+    }, {
+      name: "copy-cell",
+      label: "Copy Cell",
+      icon: "hdd"
+    }, {
+      name: "delete-cell",
+      label: "Delete Cell",
+      icon: "trash"
+    }]
+  }
+);
--- public/scripts/views/cells/controls.js
+++ public/scripts/views/cells/controls.js
@@ -1,9 +1,14 @@
-var HbsView = require('../hbs');
+/*
+  TODO: Switch mode button should change label to "Swith to Text" /
+  "Switch to Code" deopending on current mode.
+*/
+
+var Backbone      = require('backbone');
+var View          = require('../view');
 var ControlsModel = require('../../models/controls');
 
-var ControlsView = module.exports = HbsView.extend({
-  className: 'cell-controls',
-  template: require('../../../templates/controls.hbs')
+var ControlsView = module.exports = View.extend({
+  className: 'cell-controls'
 });
 
 /**
@@ -13,4 +18,17 @@ var ControlsView = module.exports = HbsView.extend({
  */
 ControlsView.prototype.initialize = function () {
   this.model = this.model || new ControlsModel();
+};
+
+ControlsView.prototype.render = function () {
+  var actionElStr;
+  var self = this;
+  this.model.actions.forEach(function (action) {
+    console.log(action);
+    actionElStr = '<button class="action-' + action.name +
+      '"><i class="icon-' + action.icon + ' icon-white"></i> ' + action.label +
+      '</button>';
+    // Parse HTML to DOM element and append to this view.
+    self.el.appendChild(Backbone.$(actionElStr)[0]);
+  });
 };
