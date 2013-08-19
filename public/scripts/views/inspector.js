d39269e4f42a5051a710308bece5cb4cc427161a
Blake Embrey <hello@blakeembrey.com>
2013-08-19 13:32:41 +1000

    Clean up inspector classes.

--- public/scripts/views/inspector.js
+++ public/scripts/views/inspector.js
@@ -1,7 +1,9 @@
-var _        = require('underscore');
-var View     = require('./view');
-var Backbone = require('backbone');
-var domify   = require('domify');
+var _         = require('underscore');
+var View      = require('./view');
+var Backbone  = require('backbone');
+var type      = require('../lib/type');
+var domify    = require('domify');
+var stringify = require('../lib/stringify');
 
 var InspectorView = module.exports = View.extend({
   className: 'inspector'
@@ -42,78 +44,8 @@ InspectorView.prototype.shouldExpand = function () {
   return _.isObject(this.inspect);
 };
 
-InspectorView.prototype.getType = function (object) {
-  // Typeof check catches the basics
-  if (typeof object === 'string')    { return 'String'; }
-  if (typeof object === 'number')    { return 'Number'; }
-  if (typeof object === 'boolean')   { return 'Boolean'; }
-  if (typeof object === 'function')  { return 'Function'; }
-  if (typeof object === 'undefined') { return 'Undefined'; }
-  // Specific object types
-  if (_.isNull(object))    { return 'Null'; }
-  if (_.isArray(object))   { return 'Array'; }
-  if (_.isDate(object))    { return 'Date'; }
-  if (_.isRegExp(object))  { return 'RegExp'; }
-  if (_.isElement(object)) { return 'Element'; }
-  // Need to check certain properties against the sandbox window
-  if (object instanceof this.context.Error) { return 'Error'; }
-  // Finally, return as a plain object
-  return 'Object';
-};
-
-InspectorView.prototype.stringifyString = function (string) {
-  return '"' + string.replace(/"/g, '\\"') + '"';
-};
-
-InspectorView.prototype.stringifyByExpansion = function (object) {
-  // If the object should be expanded to be viewed, just show the type
-  if (_.isObject(object)) { return this.getType(object); }
-  if (_.isString(object)) { return this.stringifyString(object); }
-  return '' + object;
-};
-
-InspectorView.prototype.stringifyArray = function (array) {
-  return '[' + _.map(array, function (value) {
-    return this.stringifyByExpansion(value);
-  }, this).join(', ') + ']';
-};
-
-InspectorView.prototype.stringifyObject = function (object) {
-  // Using the `keys` function to grab all the keys and then iterate, otherwise
-  // when stringifying something like the window, it tries to use numeric
-  // indexes like an array because of the `length` property.
-  var objectString = _.map(_.keys(object), function (key) {
-    var value = object[key];
-    return this.stringifyString(key) + ': ' + this.stringifyByExpansion(value);
-  }, this).join(', ');
-
-  return '{' + (objectString ? ' ' + objectString + ' ' : '') + '}';
-};
-
-InspectorView.prototype.stringifyError = function (error) {
-  // TIL DOMExceptions don't allow calling `toString` or string type coersion
-  return Error.prototype.toString.call(error);
-};
-
-InspectorView.prototype.stringifyElement = function (element) {
-  var div = document.createElement('div');
-  div.appendChild(element.cloneNode(true));
-  return div.innerHTML;
-};
-
 InspectorView.prototype.stringifyPreview = function () {
-  return this.stringify(this.inspect);
-};
-
-InspectorView.prototype.stringify = function (object) {
-  var type = this.getType(object);
-  if (type === 'Error')   { return this.stringifyError(object); }
-  if (type === 'Array')   { return this.stringifyArray(object); }
-  if (type === 'Object')  { return this.stringifyObject(object); }
-  if (type === 'String')  { return this.stringifyString(object); }
-  if (type === 'Element') { return this.stringifyElement(object); }
-  // Every other type can safely be typecasted to the expected output
-  return '' + object;
+  return stringify(this.inspect);
 };
 
 InspectorView.prototype._renderChild = function (prefix, object, special) {
@@ -187,10 +119,9 @@ InspectorView.prototype._renderChildren = function () {
 
 InspectorView.prototype.renderPreview = function () {
   var html = '';
-  var type = this.getType(this.inspect);
 
   html += '<div class="arrow"></div>';
-  html += '<div class="preview ' + type.toLowerCase() + '">';
+  html += '<div class="preview ' + type(this.inspect) + '">';
   if (_.isString(this.prefix)) {
     html += '<span class="property' + (this.special ? ' special' : '') + '">';
     html += _.escape(this.prefix);
