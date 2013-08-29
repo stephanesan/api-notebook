e904104bff7491f182bb77ca7c32b251442109e4
Blake Embrey <hello@blakeembrey.com>
2013-08-29 17:52:41 +1000

    Update hidden property recognition with a much better documented and cross frame solution for check prototypes.

--- public/scripts/lib/is-hidden-property.js
+++ public/scripts/lib/is-hidden-property.js
@@ -1,20 +1,57 @@
-var _ = require('underscore');
+var _      = require('underscore');
+var typeOf = require('./type');
+
+// Keep a reference to all the keys defined on the root object prototype.
+var objectPrototypeKeys = Object.getOwnPropertyNames(Object.prototype);
+
+// Keep a reference to all the keys on a function created by the function.
+var functionPropertyKeys = Object.getOwnPropertyNames(function () {});
 
 /**
- * Checks up the prototype chain to check if a property is enumerable.
+ * Check if the property of the object was inherited from `Object.prototype`.
+ * Please note: We can't just compare to `Object.prototype` since objects in an
+ * iFrame will have inherited from a different prototype.
  *
- * @param  {*}      object
+ * @param  {Object} object
  * @param  {String} property
  * @return {Boolean}
  */
-var isEnumerable = function (object, property) {
+var isObjectProperty = function (object, property) {
+  var obj = object;
+
   do {
-    if (object.hasOwnProperty(property)) {
-      return !object.propertyIsEnumerable(property);
+    // Use `hasOwnProperty` from the Object's prototype since the object might
+    // not have a property on it called
+    if (Object.prototype.hasOwnProperty.call(object, property)) {
+      // Do a quick check to see if we are at the end of the prototype chain. If
+      // we are, we need to compare the current object properties with
+      // `Object.prototype` since we could just be at the end of a chain started
+      // with `Object.create(null)`.
+      if (Object.getPrototypeOf(object)) { return false; }
+      // Don't check for an exact match of keys since if the prototype is from
+      // an iFrame, it could have been modified by one of those irritating JS
+      // developers that mess with prototypes directly.
+      return _.every(objectPrototypeKeys, function (property) {
+        return Object.prototype.hasOwnProperty.call(object, property);
+      });
     }
   } while (object = Object.getPrototypeOf(object));
 
-  return false;
+  return false
+};
+
+/**
+ * Check if the property of a function was inherited by the creation of the
+ * function.
+ *
+ * @param  {Function} fn
+ * @param  {String}   property
+ * @return {Boolean}
+ */
+var isFunctionProperty = function (fn, property) {
+  if (_.contains(functionPropertyKeys, property)) { return true; }
+
+  return !fn.hasOwnProperty(property);
 };
 
 /**
@@ -28,11 +65,12 @@ var isEnumerable = function (object, property) {
 module.exports = function (object, property) {
   if (typeof object === 'function' && property === '@return') { return true; }
 
-  var type = Object.prototype.toString.call(object);
-  // Plain objects created by the user and functions should not display
-  // any enumarable properties in autocompletion or the object inspector.
-  if (type === '[object Object]' || type === '[object Function]') {
-    return isEnumerable(object, property);
+  if (typeof object === 'object') {
+    return isObjectProperty(object, property);
+  }
+
+  if (typeof object === 'function') {
+    return isFunctionProperty(object, property);
   }
 
   return false;
