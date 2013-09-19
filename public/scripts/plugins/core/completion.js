485bf6cae6cee9d73965564db5e3d59ca0a85a65
Blake Embrey <hello@blakeembrey.com>
2013-09-19 10:47:28 +1000

    Keyword typo.

--- public/scripts/plugins/core/completion.js
+++ public/scripts/plugins/core/completion.js
@@ -6,7 +6,7 @@ var _ = require('underscore');
  * @type {Object}
  */
 var keywords = _.object(('break case catch continue debugger default ' +
-               'delete do else false finally for function if in instanceof' +
+               'delete do else false finally for function if in instanceof ' +
                'new null return switch throw true try typeof var void while ' +
                'with').split(' '), true);
 
