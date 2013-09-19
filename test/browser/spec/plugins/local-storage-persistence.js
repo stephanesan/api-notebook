746b418395127a01e83aa325e73624c06164e8b8
Blake Embrey <hello@blakeembrey.com>
2013-09-19 18:06:54 +1000

    Fix localStorage tests to be self-contained.

--- test/browser/spec/plugins/local-storage-persistence.js
+++ test/browser/spec/plugins/local-storage-persistence.js
@@ -1,7 +1,7 @@
 /* global describe, it, beforeEach, afterEach */
 
 describe('Local Storage Persistence Plugin', function () {
-  var id;
+  var id       = '213';
   var notebook = '# Testing localStorage';
 
   beforeEach(function () {
@@ -10,15 +10,18 @@ describe('Local Storage Persistence Plugin', function () {
 
   afterEach(function () {
     App.persistence.reset();
+    localStorage.removeItem('notebook-' + id);
     localStoragePersistencePlugin.detach(App.middleware);
   });
 
   it('should save to localStorage with a made up id', function (done) {
+    App.persistence.set('id',       id);
     App.persistence.set('notebook', notebook);
 
     App.persistence.save(function (err, content) {
       expect(content).to.equal(notebook);
-      expect(id = App.persistence.get('id')).to.exist;
+      expect(App.persistence.get('id')).to.equal(id);
+      expect(localStorage.getItem('notebook-' + id)).to.equal(notebook);
 
       done();
     });
@@ -26,11 +29,11 @@ describe('Local Storage Persistence Plugin', function () {
 
   it('should load the id from localStorage', function (done) {
     App.persistence.set('id', id);
+    localStorage.setItem('notebook-' + id, notebook);
 
     App.persistence.load(function (err, content) {
       expect(content).to.equal(notebook);
 
-      localStorage.removeItem('notebook-' + id);
       done();
     });
   });
