c2131c12ef162c3a57180497810144e53046a1de
Blake Embrey <hello@blakeembrey.com>
2013-09-21 13:14:49 +1000

    Implement raw notebook editor.

--- public/scripts/views/edit-notebook.js
+++ public/scripts/views/edit-notebook.js
@@ -1,6 +1,5 @@
-// var _           = require('underscore');
 var View        = require('./view');
-// var persistence = require('../state/persistence');
+var persistence = require('../state/persistence');
 
 /**
  * Create a new raw notebook editor instance.
@@ -8,7 +7,7 @@ var View        = require('./view');
  * @type {Function}
  */
 var EditNotebook = module.exports = View.extend({
-  className: 'edit-notebook'
+  className: 'notebook-edit'
 });
 
 /**
@@ -17,5 +16,31 @@ var EditNotebook = module.exports = View.extend({
  * @return {EditNotebook}
  */
 EditNotebook.prototype.render = function () {
+  this.editor = new CodeMirror(this.el, {
+    mode:           'gfm',
+    value:          persistence.get('contents'),
+    tabSize:        2,
+    lineNumbers:    true,
+    lineWrapping:   true,
+    viewportMargin: Infinity
+  });
 
+  // Update the persistence code every time we change the content.
+  this.listenTo(this.editor, 'change', function (cm) {
+    persistence.set('contents', cm.getValue());
+  });
+
+  return this;
+};
+
+/**
+ * Append the editor to an element and refresh the CodeMirror editor (when it's
+ * rendered off screen the view is broken).
+ *
+ * @return {EditNotebook}
+ */
+EditNotebook.prototype.appendTo = function () {
+  View.prototype.appendTo.apply(this, arguments);
+  this.editor.refresh();
+  return this;
 };
