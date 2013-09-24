437f5a048b33694c94e3f8982eb26ee82f25a488
Blake Embrey <hello@blakeembrey.com>
2013-09-24 13:42:52 +1000

    Remove cached APIs and implement path name lookups.

--- public/scripts/plugins/addons/raml-client-generator.js
+++ public/scripts/plugins/addons/raml-client-generator.js
@@ -1,6 +1,8 @@
 /* global App */
+var _               = App._;
 var ramlParser      = require('raml-parser');
 var clientGenerator = require('./lib/client-generator');
+var NOTEBOOK_URL    = process.env.NOTEBOOK_URL;
 
 /**
  * Simple map for requesting a predefined RAML document.
@@ -8,16 +10,54 @@ var clientGenerator = require('./lib/client-generator');
  * @type {Object}
  */
 var specMap = {
-  'github': process.env.NOTEBOOK_URL + '/raml/github-v3/api-single.yml'
+  box:       NOTEBOOK_URL + '/raml/box-v2.0/api-single.yml',
+  github:    NOTEBOOK_URL + '/raml/github-v3/api-single.yml',
+  stripe:    NOTEBOOK_URL + '/raml/stripe-v1/stripe-v1-single.yml',
+  quizlet:   NOTEBOOK_URL + '/raml/Quizlet-v2.0/Quizlet.yml',
+  amazonS3:  NOTEBOOK_URL + '/raml/amazon_s3/amazon_s3-single.yml',
+  linkedIn:  NOTEBOOK_URL + '/raml/linkedin-v1/linkedin-v1-single.yml',
+  instagram: NOTEBOOK_URL + '/raml/instagram-v1/api-single.yml',
+  google: {
+    contacts: NOTEBOOK_URL + '/raml/google-contacts-v3.0/gc-v3-single.yml'
+  }
 };
 
 /**
- * Simple map for tracking the return URLs against spec names. Allows names to
- * be overriden at any point and still load up the generated client.
+ * Parse a path string to a reference on the object. Supports passing an
+ * optional setter.
  *
- * @type {Object}
+ * @param  {Object} object
+ * @param  {String} path
+ * @param  {*}      [setter]
+ * @return {*}
  */
-var apiMap = {};
+var fromPath = function (object, path, setter) {
+  var isSetter = false;
+  var nodes    = path.split('.');
+
+  // Check that we have passed a third argument as the setter.
+  if (arguments.length > 2) {
+    isSetter = true;
+  }
+
+  var reference = _.reduce(nodes, function (object, prop, index) {
+    if (isSetter) {
+      // If we are at the last property, set the value.
+      if (index === nodes.length - 1) {
+        return object[prop] = setter;
+      }
+
+      // Ensure the object is available.
+      if (!(prop in object)) {
+        object[prop] = {};
+      }
+    }
+
+    return object[prop];
+  }, object);
+
+  return reference;
+};
 
 /**
  * Responsible for loading RAML documents and return API clients.
@@ -27,35 +67,27 @@ var apiMap = {};
  * @param {Function} done
  */
 var Api = function (name /*, url */, done) {
-  var url = specMap[name];
+  var url = fromPath(specMap, name);
 
   // Allows overloading the middle parameter with a url for the RAML loader.
   if (arguments.length > 1 && typeof arguments[1] !== 'function') {
-    url  = specMap[name] = arguments[1];
+    url  = fromPath(specMap, name, arguments[1]);
     done = arguments[2];
   }
 
   App._executeContext.timeout(Infinity);
   done = done || App._executeContext.async();
 
-  // Skip loading the document if the name and URL has already been processed.
-  if (specMap[name] === apiMap[name]) {
-    return process.nextTick(function () {
-      return done(null, Api[name]);
-    });
-  }
-
   // Pass our url to the RAML parser for processing and transform the promise
   // back into a callback format.
   ramlParser.loadFile(url).then(function (data) {
     try {
-      apiMap[name] = url;
-      Api[name]    = clientGenerator(data);
+      fromPath(Api, name, clientGenerator(data));
     } catch (e) {
       return done(e);
     }
 
-    return done(null, Api[name]);
+    return done(null, fromPath(Api, name));
   }, function (err) {
     return done(err);
   });
