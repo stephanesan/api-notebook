63bba0629739a16506958272d7e7d45ae9c8d7dd
Blake Embrey <hello@blakeembrey.com>
2013-10-05 14:10:01 +1000

    Add some document function returns.

--- public/scripts/plugins/core/lib/function-returns.js
+++ public/scripts/plugins/core/lib/function-returns.js
@@ -1,7 +1,7 @@
 var _ = require('underscore');
 
 /**
- * Resolve functions on the prototype. Implements an O(n) lookup time.
+ * Resolve functions on the prototype. Implements an O(1) lookup time.
  *
  * @param  {Object} options
  * @param  {Object} prototype
@@ -81,16 +81,6 @@ module.exports = function (options) {
       return prototypeResolve(options, _.pick(Date.prototype, dateProperties));
     }
 
-    // Because, why not?
-    if (options.fn === global.Date.now) {
-      return Date.now();
-    }
-
-    /**
-     * Functions below this point require arguments to be 100% useful, but for
-     * the most part it won't hurt to display basic suggestions.
-     */
-
     /**
      * Functions that return strings.
      *
@@ -105,14 +95,17 @@ module.exports = function (options) {
       global.decodeURI,
       global.encodeURIComponent,
       global.decodeURIComponent,
-      // Arrays.
+      // Array functions.
       global.Array.prototype.join,
       // Built-in `toString` methods.
       global.Array.prototype.toString,
       global.Error.prototype.toString,
       global.RegExp.prototype.toString,
       global.Object.prototype.toString,
-      global.Object.prototype.toLocaleString
+      global.Object.prototype.toLocaleString,
+      // Element functions.
+      global.Element.prototype.getAttribute,
+
     ];
 
     /**
@@ -127,12 +120,12 @@ module.exports = function (options) {
       global.parseFloat,
       global.setTimeout,
       global.setInterval,
-      // Arrays.
+      // Array functions.
       global.Array.prototype.push,
       global.Array.prototype.indexOf,
       global.Array.prototype.unshift,
       global.Array.prototype.lastIndexOf,
-      // Math object.
+      // Math object functions.
       global.Math.abs,
       global.Math.cos,
       global.Math.exp,
@@ -153,8 +146,11 @@ module.exports = function (options) {
       global.Math.round,
       global.Math.random,
       // Date functions.
+      global.Date.now,
       global.Date.UTC,
-      global.Date.parse
+      global.Date.parse,
+      // Document functions.
+      global.Node.prototype.compareDocumentPosition
     ];
 
     /**
@@ -167,20 +163,32 @@ module.exports = function (options) {
       global.isNaN,
       global.Boolean,
       global.isFinite,
-      // Arrays.
+      // Array functions.
       global.Array.isArray,
       global.Array.prototype.some,
       global.Array.prototype.every,
-      // Regular Expressions.
+      // Regular Expression functions.
       global.RegExp.prototype.test,
-      // Objects.
+      // Object functions.
       global.Object.is,
       global.Object.isSealed,
       global.Object.isFrozen,
       global.Object.isExtensible,
       global.Object.prototype.isPrototypeOf,
       global.Object.prototype.hasOwnProperty,
-      global.Object.prototype.propertyIsEnumerable
+      global.Object.prototype.propertyIsEnumerable,
+      // Document functions.
+      global.HTMLDocument.prototype.hasFocus,
+      // DOM node functions.
+      global.Node.prototype.contains,
+      global.Node.prototype.isSameNode,
+      global.Node.prototype.isEqualNode,
+      global.Node.prototype.isSupported,
+      global.Node.prototype.hasChildNodes,
+      global.Node.prototype.isDefaultNamespace,
+      // Element functions.
+      global.Element.prototype.hasAttribute,
+      global.Element.prototype.hasAttributes
     ];
 
     /**
@@ -189,7 +197,7 @@ module.exports = function (options) {
      * @type {Array}
      */
     var arrayTypes = [
-      // Arrays.
+      // Array functions.
       global.Array,
       global.Array.prototype.map,
       global.Array.prototype.sort,
@@ -198,9 +206,9 @@ module.exports = function (options) {
       global.Array.prototype.concat,
       global.Array.prototype.filter,
       global.Array.prototype.reverse,
-      // Regular Expressions.
+      // Regular Expression functions.
       global.RegExp.prototype.exec,
-      // Objects.
+      // Object functions.
       global.Object.keys,
       global.Object.getOwnPropertyNames
     ];
@@ -211,14 +219,53 @@ module.exports = function (options) {
      * @type {Array}
      */
     var objectTypes = [
-      // Objects.
+      // Object functions.
       global.Object.seal,
       global.Object.create,
       global.Object.freeze,
       global.Object.getPrototypeOf,
       global.Object.preventExtensions,
       global.Object.prototype.valueOf,
-      global.Object.getOwnPropertyDescriptor
+      global.Object.getOwnPropertyDescriptor,
+      // Document functions.
+      global.Document.prototype.getSelection,
+      global.Document.prototype.caretRangeFromPoint,
+      // Element functions.
+      global.Element.prototype.getClientRects,
+      global.Element.prototype.getBoundingClientRect
+    ];
+
+    /**
+     * Functions that return a NodeList instance.
+     *
+     * @type {Array}
+     */
+    var nodeListTypes = [
+      // Document functions.
+      global.Document.prototype.querySelectorAll,
+      global.Document.prototype.getElementsByName,
+      global.Document.prototype.getElementsByTagName,
+      global.Document.prototype.getElementsByTagNameNS,
+      global.Document.prototype.getElementsByClassName,
+      // Element functions.
+      global.Element.prototype.querySelectorAll,
+      global.Element.prototype.getElementsByTagName,
+      global.Element.prototype.getElementsByTagNameNS,
+      global.Element.prototype.getElementsByClassName
+    ];
+
+    /**
+     * Functions that return a Node instance.
+     *
+     * @type {Array}
+     */
+    var nodeTypes = [
+      // Node functions.
+      global.Node.prototype.cloneNode,
+      global.Node.prototype.appendChild,
+      global.Node.prototype.removeChild,
+      global.Node.prototype.replaceChild,
+      global.Node.prototype.insertBefore,
     ];
 
     if (_.contains(stringTypes, options.fn)) {
@@ -240,14 +287,56 @@ module.exports = function (options) {
     if (_.contains(objectTypes, options.fn)) {
       return new options.global.Object();
     }
+
+    if (_.contains(nodeTypes, options.fn)) {
+      return global.Node.prototype;
+    }
+
+    if (_.contains(nodeListTypes, options.fn)) {
+      return global.NodeList.prototype;
+    }
+
+    /**
+     * DOM node creation functions.
+     */
+
+    if (options.fn === global.Document.prototype.createAttribute) {
+      return global.Attr.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createTextNode) {
+      return global.Text.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createComment) {
+      return global.Comment.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createDocumentFragment) {
+      return global.DocumentFragment.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createElement) {
+      return global.Element.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createExpression) {
+      return global.XPathExpression.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createRange) {
+      return global.Range.prototype;
+    }
+
+    if (options.fn === global.Document.prototype.createNSResolver) {
+      return global.XPathNSResolver.prototype;
+    }
+
+    return null;
   }
 
   // If the variable/property is a constructor function, we can provide
   // some additional context by looking at the `prototype` property. This will
   // only run when all other completion options have failed.
-  if (options.isConstructor) {
-    return options.fn.prototype;
-  }
-
-  return null;
+  return options.fn.prototype;
 };
