32d1593fff30358b1198e5b4775081a78a8d97c7
Blake Embrey <hello@blakeembrey.com>
2013-10-08 13:23:42 +1000

    Tooltip rendering.

--- public/scripts/lib/codemirror/sandbox-tooltip.js
+++ public/scripts/lib/codemirror/sandbox-tooltip.js
@@ -1,4 +1,5 @@
 var _            = require('underscore');
+var Pos          = CodeMirror.Pos;
 var getToken     = require('./get-token');
 var middleware   = require('../../state/middleware');
 var tokenHelpers = require('./token-helpers');
@@ -11,14 +12,33 @@ var tokenHelpers = require('./token-helpers');
  * @return {Object}
  */
 var getPrevBracket = function (cm, token) {
+  var level = 0;
+
   do {
-    // Break the loop if we have found a bracket token.
-    if (token.type === null && token.string === '(') {
-      return token;
+    if (token.string === '(') {
+      // Break the loop if we have found the matching bracket token.
+      if (level === 0) {
+        return token;
+      }
+
+      level--;
+    } else if (token.string === ')') {
+      level++;
     }
   } while (token = tokenHelpers.getPrevToken(cm, token));
 
+  // By the time we hit here, we'll be returning `null`.
   return token;
+};
+
+/**
+ * Cache the current token and return data for the lookup token.
+ *
+ * @type {Object}
+ */
+var cache = {
+  data:  null,
+  token: null
 };
 
 /**
@@ -36,23 +56,56 @@ module.exports = function (cm, options, done) {
   }
 
   var cur     = cm.getCursor();
-  var bracket = getPrevBracket(cm, getToken(cm, cur));
+  var token   = getToken(cm, cur);
+  var bracket = getPrevBracket(cm, token);
   var before  = bracket && tokenHelpers.eatSpaceAndMove(cm, bracket);
 
   // No token before the bracket can be found.
   if (!before) {
+    cache.data  = null;
+    cache.token = null;
     return done();
   }
 
+  // Check with the current cache object and serve the previous result if
+  // possible.
+  if (cache.token) {
+    var cToken    = cache.token;
+    var cachePos  = cToken.pos;
+    var beforePos = before.pos;
+
+    // Position is the same.
+    if (beforePos.ch === cachePos.ch && beforePos.line === cachePos.line) {
+      // Tokens are the same.
+      if (before.type === cToken.type && before.string === cToken.string) {
+        // Update the `to` position (where the cursor currently is) and return.
+        return done(null, _.extend(cache.data, {
+          to: cur
+        }));
+      }
+    }
+  }
+
   return tokenHelpers.getProperty(cm, before, options, function (err, data) {
-    if (err) { return done(err); }
+    if (err || !_.isFunction(data.context)) {
+      return done(err);
+    }
 
     // If the context is a function, let's get the data available for the
     // tooltip and return it.
-    if (_.isFunction(data.context)) {
-      middleware.trigger('completion:describe', data, function (err, describe) {
-        console.log(err, describe);
-      });
-    }
+    middleware.trigger('completion:describe', data, function (err, describe) {
+      // Cache the data since we'll be calling the method regular with no real
+      // changes in argument positions or types.
+      cache.token = before;
+      cache.data  = {
+        token:       token,
+        context:     data.context,
+        description: describe,
+        to:          cur,
+        from:        new Pos(cur.line, token.start)
+      };
+
+      return done(err, cache.data);
+    });
   });
 };
--- public/scripts/lib/completion/tooltip.js
+++ public/scripts/lib/completion/tooltip.js
@@ -1,3 +1,6 @@
+var _     = require('underscore');
+var state = require('../../state/state');
+
 /**
  * Renders a documentation tooltip.
  *
@@ -11,4 +14,92 @@ var Tooltip = module.exports = function (completion, data) {
   this.completion = completion;
 
   CodeMirror.signal(completion.cm, 'startTooltip', completion.cm);
+
+  this.render();
+};
+
+/**
+ * Render the tooltip based on the description object.
+ *
+ * @return {Tooltip}
+ */
+Tooltip.prototype.render = function () {
+  this.removeTooltip();
+
+  if (!this.data.description['!type']) {
+    return this;
+  }
+
+  this._tooltip = document.createElement('div');
+  this._tooltip.className = 'CodeMirror-tooltip';
+
+  document.body.appendChild(this._tooltip);
+
+  var typeEl = this._tooltip.appendChild(document.createElement('div'));
+  typeEl.className = 'CodeMirror-tooltip-type';
+  typeEl.appendChild(document.createTextNode(this.data.description['!type']));
+
+  this.reposition();
+
+  // Listen to changes in the viewport size and reposition the tooltip.
+  state.on('change:window.width', this.onResize = _.bind(
+    this.reposition, this
+  ));
+
+  return this;
+};
+
+/**
+ * Position the tooltip in the window.
+ *
+ * @return {[type]} [description]
+ */
+Tooltip.prototype.reposition = function () {
+  var pos = this.completion.cm.cursorCoords(this.data.to);
+
+  this._tooltip.style.position = 'absolute';
+  this._tooltip.style.top      = pos.bottom + 'px';
+  this._tooltip.style.left     = pos.left   + 'px';
+
+  var tooltip    = this._tooltip.getBoundingClientRect();
+  var winWidth   = state.get('window.width');
+  var rightWidth = tooltip.right - tooltip.left;
+
+  if (tooltip.right > winWidth - 5 && tooltip.left > rightWidth) {
+    this._tooltip.className += ' CodeMirror-tooltip-right';
+    this._tooltip.style.left  = 'auto';
+    this._tooltip.style.right = winWidth - pos.left + 'px';
+  }
+
+  return this;
+};
+
+/**
+ * Destroy the tooltip DOM element.
+ *
+ * @return {Tooltip}
+ */
+Tooltip.prototype.removeTooltip = function () {
+  if (this._tooltip) {
+    this._tooltip.parentNode.removeChild(this._tooltip);
+    delete this._tooltip;
+  }
+
+  state.off('change:window.width', this.onResize);
+  delete this.onResize;
+
+  return this;
+};
+
+/**
+ * Remove the tooltip from the DOM.
+ *
+ * @return {Tooltip}
+ */
+Tooltip.prototype.remove = function () {
+  this.removeTooltip();
+
+  CodeMirror.signal(this.completion.cm, 'endTooltip', this.completion.cm);
+
+  return this;
 };
