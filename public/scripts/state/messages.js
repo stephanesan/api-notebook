fe3d2b541097f4b140d43fd0036998b54124e6ba
Blake Embrey <hello@blakeembrey.com>
2014-04-24 15:25:08 +1000

    Remove attempt to block CodeMirror refreshes

--- public/scripts/state/messages.js
+++ public/scripts/state/messages.js
@@ -13,18 +13,7 @@ var messages = module.exports = _.extend({}, Backbone.Events);
 /**
  * Proxy resize events to the current state.
  */
-messages.listenTo(domListen(window), 'resize', function (e) {
-  /**
-   * Every time the window triggers a resize, stop it from propagating so
-   * CodeMirror doesn't recalculate itself.
-   *
-   * TODO: Work out why CodeMirror does this and it sucks so much processing
-   * powering. For reference, wrapping try..catch quadruples execution time and
-   * additional unnecessary resize events are being triggered from the parent
-   * window adjusting the iframe size.
-   */
-  e.stopImmediatePropagation();
-
+messages.listenTo(domListen(window), 'resize', _.throttle(function () {
   // Trigger a resize message to the parent frame.
   messages.trigger('resize');
-});
+}, 60));
